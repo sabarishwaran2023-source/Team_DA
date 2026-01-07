5164b64962e51ca2125f5c9f6b21ef2b92867d0e
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-01-07 19:03:19 +0530

    da1

---
+++ da.py
@@ -0,0 +1,10 @@
+
+a = 5
+b = 7
+sum = a + b
+print("The sum is:", sum)
+
+# Option 2: User input
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+print("The sum is:", x + y)
